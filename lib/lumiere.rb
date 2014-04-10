ae4104c2586b5b1305c64949fb8a85280cf39e5c
Karl Entwistle <karl.entwistle@unboxedconsulting.com>
2014-04-10 17:58:16 +0100

    Refactor fetch syntax

--- lib/lumiere.rb
+++ lib/lumiere.rb
@@ -6,8 +6,8 @@ Dir[File.dirname(__FILE__) + '/provider/*.rb'].each {|file| require file }
 
 module Lumiere
 
-  def self.fetch(video, &block)
-    block.call(remote_structure(video.api_url))
+  def self.fetch(video)
+    yield remote_structure(video.api_url)
   end
 
   private
