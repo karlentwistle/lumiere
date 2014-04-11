fd48eceba4779c94cb1311cd0358434a594d4744
Karl Entwistle <karl.entwistle@unboxedconsulting.com>
2014-04-11 11:28:01 +0100

    Remove duplicated lets that are already within scope

--- spec/provider/vimeo_spec.rb
+++ spec/provider/vimeo_spec.rb
@@ -5,10 +5,12 @@ module Lumiere
     let(:description) { 'Description of my awesome Video' }
     let(:title) { 'Title of my awesome Video' }
     let(:remote_structure) {
-      [{
-        'title' => title,
-        'description' => description
-      }]
+      [
+        {
+          'title' => title,
+          'description' => description
+        }
+      ]
     }
 
     it "stores id" do
@@ -23,7 +25,6 @@ module Lumiere
     end
 
     describe "#title" do
-      let(:title) { 'Title of Video' }
       it "returns the video title" do
         Lumiere.stub(:remote_structure) { remote_structure }
         expect(Vimeo.new('VIDEO_ID').title).to eql(title)
@@ -31,7 +32,6 @@ module Lumiere
     end
 
     describe "#description" do
-      let(:description) { 'Description of my awesome Video' }
       it "returns the video description" do
         Lumiere.stub(:remote_structure) { remote_structure }
         expect(Vimeo.new('VIDEO_ID').description).to eql(description)
--- spec/provider/youtube_spec.rb
+++ spec/provider/youtube_spec.rb
@@ -29,7 +29,6 @@ module Lumiere
     end
 
     describe "#title" do
-      let(:title) { 'Title of Video' }
       it "returns the video title" do
         Lumiere.stub(:remote_structure) { remote_structure }
         expect(YouTube.new('VIDEO_ID').title).to eql(title)
@@ -37,7 +36,6 @@ module Lumiere
     end
 
     describe "#description" do
-      let(:description) { 'Description of my awesome Video' }
       it "returns the video description" do
         Lumiere.stub(:remote_structure) { remote_structure }
         expect(YouTube.new('VIDEO_ID').description).to eql(description)
