9d7ec7bd4d1e2325d5f205b7f282446b51961364
Karl Entwistle <karl.entwistle@unboxedconsulting.com>
2014-04-13 21:57:05 +0100

    Remote Response
    * Fetches a raw_response from a URL
    * Parses the raw_response with the parser passed

---
+++ lib/remote_response.rb
@@ -0,0 +1,19 @@
+module Lumiere
+  class RemoteResponse
+    def initialize(url, parser)
+      @url = url
+      @parser = parser
+    end
+
+    def parse
+      @parser.parse(raw_response)
+    end
+
+    private
+
+    def raw_response
+      open(@url).read
+    end
+
+  end
+end
---
+++ spec/remote_response_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+module Lumiere
+  describe RemoteResponse do
+    describe "#parse" do
+      let(:url) { double }
+      let(:parser) { double }
+      let(:raw_response) { double(read: true) }
+
+      subject(:remote_response) { RemoteResponse.new(url, parser) }
+
+      it "calls parse on parser" do
+        allow(remote_response).to receive(:open).with(url) { raw_response }
+        expect(parser).to receive(:parse).with { raw_response }
+        remote_response.parse
+      end
+    end
+  end
+end
