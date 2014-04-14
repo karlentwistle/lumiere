4dccaa188f180c94a6d008d82c35f15069927555
Karl Entwistle <karl.entwistle@unboxedconsulting.com>
2014-04-14 11:38:13 +0100

    Expose schemeless_parse as an instance method and a class method

--- lib/extended_uri.rb
+++ lib/extended_uri.rb
@@ -1,5 +1,17 @@
 module Lumiere
   module ExtendedURI
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def schemeless_parse(url)
+        uri = URI.parse(url)
+        uri = URI.parse("http://#{url}") if uri.scheme.nil?
+        uri
+      end
+    end
+
     def schemeless_parse(url)
       uri = URI.parse(url)
       uri = URI.parse("http://#{url}") if uri.scheme.nil?
--- spec/extended_uri_spec.rb
+++ spec/extended_uri_spec.rb
@@ -30,6 +30,35 @@ module Lumiere
           expect(schemeless_parse.to_s).to eql('http://example.com/thing')
         end
       end
+    end
+
+    describe ".schemeless_parse" do
+      let(:dummy_class) { class Thing; include ExtendedURI; end }
+      subject(:schemeless_parse) { dummy_class.schemeless_parse(url) }
+
+      context 'url contains http' do
+        let(:url) { 'http://example.com/' }
+        it "returns a uri parse object with url unmodified" do
+          expect(schemeless_parse).to be_a(URI)
+          expect(schemeless_parse.to_s).to eql('http://example.com/')
+        end
+      end
+
+      context 'url contains https' do
+        let(:url) { 'https://example.com/' }
+        it "returns a uri parse object with url unmodified" do
+          expect(schemeless_parse).to be_a(URI)
+          expect(schemeless_parse.to_s).to eql('https://example.com/')
+        end
+      end
+
+      context 'url contains no scheme' do
+        let(:url) { 'example.com/thing' }
+        it "returns a uri parse object with url modified to include http" do
+          expect(schemeless_parse).to be_a(URI)
+          expect(schemeless_parse.to_s).to eql('http://example.com/thing')
+        end
+      end
 
     end
   end
