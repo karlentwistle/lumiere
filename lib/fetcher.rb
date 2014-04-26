288ccf9ef35547440416bc67273281bbd3457546
Karl Entwistle <karl.entwistle@unboxedconsulting.com>
2014-04-26 20:13:26 +0100

    Expose fetched as a public method

--- lib/fetcher.rb
+++ lib/fetcher.rb
@@ -1,19 +1,16 @@
 module Lumiere
 class Fetcher
 
-  attr_accessor :fetched
-
   def initialize(api_url, unpack_into)
     @api_url = api_url
     @unpack_into = unpack_into
-    @fetched = fetch
+  end
+
+  def fetched
+    @unpack_into.from_json(raw_response)
   end
 
   private
-
-  def fetch
-    @unpack_into.from_json(raw_response)
-  end
 
   def raw_response
     open(@api_url).read
