298516230b1bcf6341d96a6043aa6d09ce38b486
Karl Entwistle <karl.entwistle@unboxedconsulting.com>
2014-05-07 10:26:49 +0100

    Embed code generate
    * Takes a src link and options and returns an <iframe> html object

---
+++ lib/embed_code.rb
@@ -0,0 +1,33 @@
+module Lumiere
+  module EmbedCode
+    extend self
+
+    def generate(src, opts={})
+      opts ||= {}
+      opts = {src: src}.merge(opts)
+
+      generate_html_object('iframe', opts)
+    end
+
+    private
+
+    def generate_html_object(object_name, opts)
+      html_options = generate_object_properties(opts)
+      "<#{object_name} #{html_options}></#{object_name}>"
+    end
+
+    def generate_object_properties(opts={})
+      opts.map do |key, value|
+        generate_object_property(key, value)
+      end.join(' ')
+    end
+
+    def generate_object_property(key, value)
+      if value.is_a?(TrueClass)
+        value = key #set as option=option rather than option=true
+      end
+
+      "#{key}=\"#{value}\""
+    end
+  end
+end
---
+++ spec/embed_code_spec.rb
@@ -0,0 +1,41 @@
+require_relative '../lib/embed_code'
+
+module Lumiere
+  describe EmbedCode do
+
+    describe ".generate" do
+      subject(:pages) { (EmbedCode.generate('/remote', opts)) }
+
+      context "no options" do
+        let(:opts) { nil }
+        it 'returns iframe with src' do
+          expect(pages).to eql('<iframe src="/remote"></iframe>')
+        end
+      end
+
+      context "single option passed" do
+        let(:opts) { {width: 200} }
+        it 'returns iframe with src and option' do
+          expect(pages).to eql('<iframe src="/remote" width="200"></iframe>')
+        end
+      end
+
+      context "multiple options passed" do
+        let(:opts) { {width: 200, height: 300} }
+        it 'returns iframe with src and options' do
+          expect(pages).to eql('<iframe src="/remote" width="200" height="300"></iframe>')
+        end
+      end
+
+      context "boolean option passed" do
+        context "true" do
+          let(:opts) { {allowfullscreen: true} }
+          it 'returns iframe with src and boolean option set as option=option rather than option=true' do
+            expect(pages).to eql('<iframe src="/remote" allowfullscreen="allowfullscreen"></iframe>')
+          end
+        end
+      end
+    end
+
+  end
+end
