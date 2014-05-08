73541536051d0ce6ae7565a93761157a26b36127
Karl Entwistle <karl.entwistle@unboxedconsulting.com>
2014-05-08 12:54:23 +0100

    Refactor embed_code

--- lib/embed_code/embed_code.rb
+++ lib/embed_code/embed_code.rb
@@ -15,15 +15,16 @@ module Lumiere
       object_properties = default_iframe_attributes.merge(iframe_attributes)
       url_properties = default_url_attributes.merge(url_attributes)
 
-      generate(embed_url, url_properties, object_properties)
+      src = src_encoder.encode(embed_url, url_properties)
+      object_properties = {src: src}.merge(object_properties)
+      generate_html_object('iframe', object_properties)
     end
 
     private
 
-    def generate(src, url_properties={}, object_properties={})
-      src = src_encoder.encode(src, url_properties)
-      object_properties = {src: src}.merge(object_properties)
-      generate_html_object('iframe', object_properties)
+    def generate_html_object(object_name, opts)
+      html_options = html_object_properties.generate(opts)
+      "<iframe #{html_options}></iframe>"
     end
 
     def src_encoder
@@ -32,11 +33,6 @@ module Lumiere
 
     def html_object_properties
       HTMLObjectProperties
-    end
-
-    def generate_html_object(object_name, opts)
-      html_options = html_object_properties.generate(opts)
-      "<#{object_name} #{html_options}></#{object_name}>"
     end
   end
 end
