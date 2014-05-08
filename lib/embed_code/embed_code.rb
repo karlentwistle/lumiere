419d6bf6994a99513317ecbe34c899b3d141e2f5
Karl Entwistle <karl.entwistle@unboxedconsulting.com>
2014-05-08 13:25:24 +0100

    Refactor embed_code

--- lib/embed_code/embed_code.rb
+++ lib/embed_code/embed_code.rb
@@ -6,29 +6,35 @@ module Lumiere
     extend self
 
     def embed_code(opts = {})
-      iframe_attributes = opts.fetch(:iframe_attributes, {})
-      url_attributes = opts.fetch(:url_attributes, {})
+      src = generate_src(opts)
+      object_properties = generate_object_properties(opts)
 
-      default_iframe_attributes = default_attributes.fetch(:iframe_attributes, {})
-      default_url_attributes = default_attributes.fetch(:url_attributes, {})
-
-      object_properties = default_iframe_attributes.merge(iframe_attributes)
-      url_properties = default_url_attributes.merge(url_attributes)
-
-      src = src_encoder.(embed_url, url_properties)
-      object_properties = {src: src}.merge(object_properties)
-      generate_html_object('iframe', object_properties)
+      generate_iframe(src, object_properties)
     end
 
     private
 
-    def generate_html_object(object_name, opts)
-      html_options = html_object_properties.(opts)
-      "<iframe #{html_options}></iframe>"
+    def generate_src(opts = {})
+      url_attributes = opts.fetch(:url_attributes, {})
+      default_url_attributes = default_attributes.fetch(:url_attributes, {})
+      url_properties = default_url_attributes.merge(url_attributes)
+      src_encoder.(embed_url, url_properties)
     end
 
     def src_encoder
       SRC
+    end
+
+    def generate_object_properties(opts = {})
+      iframe_attributes = opts.fetch(:iframe_attributes, {})
+      default_iframe_attributes = default_attributes.fetch(:iframe_attributes, {})
+      default_iframe_attributes.merge(iframe_attributes)
+    end
+
+    def generate_iframe(src, opts)
+      opts = {src: src}.merge(opts)
+      html_options = html_object_properties.(opts)
+      "<iframe #{html_options}></iframe>"
     end
 
     def html_object_properties
